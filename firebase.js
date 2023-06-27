53c8bed1b989d8729ef99dfd755fdd46efed0925
laurenratcliffe <ratcliffelau@gmail.com>
2023-06-26 21:04:41 -0400

    firebase set up

---
+++ firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+
+export const firebaseConfig = {
+apiKey: "AIzaSyB15pHBx9Z8vwYjGZ2wONyW2bn6ofEYRhw",
+authDomain: "novas-app-b5b59.firebaseapp.com",
+projectId: "novas-app-b5b59",
+storageBucket: "novas-app-b5b59.appspot.com",
+messagingSenderId: "724798170484",
+appId: "1:724798170484:web:fbce327dd904262f9eb821"
+};
+
+export const app = initializeApp(firebaseConfig);
+
+    
+
